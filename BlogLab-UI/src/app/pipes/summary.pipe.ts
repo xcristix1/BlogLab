51d38de832e9118295d7782d70ac4f99a6ed3194
xcristix1 <uncristian10@gmail.com>
2021-05-25 06:02:10 +0300

    altceva

---
+++ BlogLab-UI/src/app/pipes/summary.pipe.ts
@@ -0,0 +1,12 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'summary'
+})
+export class SummaryPipe implements PipeTransform {
+
+  transform(value: unknown, ...args: unknown[]): unknown {
+    return null;
+  }
+
+}
